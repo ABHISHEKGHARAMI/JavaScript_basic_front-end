c6c72aa6e470b2f95d7c9689de2f3ea98f6179ba
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-05-07 12:29:56 +0530

    mod .

---
+++ core-javascript/js1_lex.js
@@ -0,0 +1,17 @@
+/*
+
+This is the first core javascript  for lexicographic order for the language .
+
+Javascript has two types of datatypes
+
+1. premitive : - Strings , Number etc
+2. Objects :- Class , Objects
+
+
+*/
+
+var s1 = "brown fox jump on the lazy dog."
+
+console.log(s1);
+
+console.log("the length of the string is :" + s1.length);

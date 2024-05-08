08fdfe0b4e0ce71186ba3362e4d6033b74593af6
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-05-08 15:52:28 +0530

    Adding the number for the repo .

---
+++ core-javascript/number1.js
@@ -0,0 +1,10 @@
+// number
+const logger = require('../logger');
+let x=3;
+let y=4;
+
+var data = Math.pow(x,y);
+
+console.log(`The power of the ${x} and ${y} is : ${data}`);
+
+logger.log('INFO',`The power of the ${x} and ${y} is : ${data}`)

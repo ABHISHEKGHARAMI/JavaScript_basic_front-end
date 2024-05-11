9f2132d54f625cb1626b0c37d4eadbc2eea360f4
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-05-11 11:48:52 +0530

    mod .

---
+++ core-javascript/datatypes1.js
@@ -0,0 +1,17 @@
+/* 
+
+There are 8 different type of datatypes are present in the javascript
+
+1. Number
+2. BigInt
+3. String
+4. Boolean
+5. Null value
+6. Undefined value
+7. symbol
+8. object
+
+Here we go for one by one .
+
+
+ */

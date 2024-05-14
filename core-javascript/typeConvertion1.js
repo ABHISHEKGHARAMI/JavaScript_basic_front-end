2b884d2e2236e251bb37ea131bb22d5e7284c4ee
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-05-14 12:24:03 +0530

    After converting the it in to string .

--- core-javascript/typeConvertion1.js
+++ core-javascript/typeConvertion1.js
@@ -4,6 +4,14 @@ const logger = require('../logger');
 
 // string convertion
 let val1 = true;
+console.log('before convertion :');
+logger.log("before convertion :")
 console.log(`the type of the ${val1} is : ${typeof(val1)}`);
 logger.log('INFO',`the type of the ${val1} is : ${typeof(val1)}`);
 
+// now convert the val1 to string
+val1 = String(val1)
+console.log("After  convertion :");
+logger.log('INFO',"After convertion :")
+console.log(`the type of the ${val1} is : ${typeof(val1)}`);
+logger.log('INFO',`the type of the ${val1} is : ${typeof(val1)}`);

4de26cfb9bd4dee653f7c4d94bd85bb0c6f264ad
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-05-14 16:43:49 +0530

    mod .

--- core-javascript/typeConvertion1.js
+++ core-javascript/typeConvertion1.js
@@ -5,7 +5,7 @@ const logger = require('../logger');
 // string convertion
 let val1 = true;
 console.log('before convertion :');
-logger.log("before convertion :")
+logger.log("before convertion :");
 console.log(`the type of the ${val1} is : ${typeof(val1)}`);
 logger.log('INFO',`the type of the ${val1} is : ${typeof(val1)}`);
 

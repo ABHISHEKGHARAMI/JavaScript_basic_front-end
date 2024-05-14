81997ae0b3b21570a8beb155e72340b5095a0f32
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-05-14 16:55:09 +0530

    mod .

--- core-javascript/operations1.js
+++ core-javascript/operations1.js
@@ -12,3 +12,13 @@ x = -x;
 
 console.log(`After Urinary operation the value will be : ${x}`);
 logger.log('INFO',`After Urinary operation the value will be : ${x}`);
+
+
+// 2: Binary Operation
+
+let y = 3;
+
+let val = y - x;
+
+console.log(`the value will be : ${val}`);
+logger.log('INFO',`the value will be : ${val}`)

cee3942bc64e040b3fbe39ad9816d4523a1024a4
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-05-14 12:38:20 +0530

    Number convertion.

--- core-javascript/typeConvertion1.js
+++ core-javascript/typeConvertion1.js
@@ -15,3 +15,19 @@ console.log("After  convertion :");
 logger.log('INFO',"After convertion :")
 console.log(`the type of the ${val1} is : ${typeof(val1)}`);
 logger.log('INFO',`the type of the ${val1} is : ${typeof(val1)}`);
+
+
+// number convertion
+
+let numStr1 = "123";
+
+console.log(`the ${numStr1} is : ${typeof(numStr1)}`);
+logger.log('INFO',`the ${numStr1} is : ${typeof(numStr1)}`);
+
+
+// after convertion
+
+numStr1 = Number(numStr1);
+
+console.log(`the ${numStr1} is ${typeof(numStr1)}`);
+logger.log('INFO',`After convertion the type of ${numStr1} is : ${typeof(numStr1)}`);

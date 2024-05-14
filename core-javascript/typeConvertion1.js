ece10a1b7d52fcf3bc1b2b6cc5810d1d0d6e626c
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-05-14 12:43:14 +0530

    Boolean convertion .

--- core-javascript/typeConvertion1.js
+++ core-javascript/typeConvertion1.js
@@ -31,3 +31,21 @@ numStr1 = Number(numStr1);
 
 console.log(`the ${numStr1} is ${typeof(numStr1)}`);
 logger.log('INFO',`After convertion the type of ${numStr1} is : ${typeof(numStr1)}`);
+
+
+
+// boolean convertion
+let bool1 = 1;
+let bool2 = 0;
+
+
+console.log(`the type of the ${bool1} is : ${typeof(bool1)} and the type of the ${bool2} is : ${typeof(bool2)}`);
+logger.log('INFO',`the type of the ${bool1} is : ${typeof(bool1)} and the type of the ${bool2} is : ${typeof(bool2)}`);
+
+
+//After convertion 
+
+bool1 = Boolean(bool1);
+bool2 = Boolean(bool2);
+console.log(`the type of the ${bool1} is : ${typeof(bool1)} and the type of the ${bool2} is : ${typeof(bool2)}`);
+logger.log('INFO',`the type of the ${bool1} is : ${typeof(bool1)} and the type of the ${bool2} is : ${typeof(bool2)}`);

19936294bc3ac45591b5da3b8edabfbb4faa48c1
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-05-14 10:58:15 +0530

    Adding the readline for the repo .

--- core-javascript/propmt1.js
+++ core-javascript/propmt1.js
@@ -1,9 +1,17 @@
 // setting up the logger
 
 const logger = require('../logger');
+const readline = require('readline');
 
 
-let name = prompt("enter the name :"," ");
+// adding the readline
+const r1 = readline.createInterface({
+    input: process.stdin,
+    output : process.stdout
+});
 
-console.log(`The name of name is : ${name}`);
-logger.log('INFO',`the name of the gentleman is : ${name}`);
+r1.question('enter the name ?',(name)=>{
+    console.log(`the name is : ${name}`);
+    logger.log('INFO',`the name of the gentleman is : ${name}`);
+    r1.close();
+})

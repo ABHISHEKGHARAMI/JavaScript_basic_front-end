385f9163537ef0aa5e77bf92f9de73c9dd167b0f
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-05-18 12:20:35 +0530

    mod .

---
+++ core-javascript/logicalOperators1.js
@@ -0,0 +1,22 @@
+// import logger
+
+const logger = require('../logger');
+
+/*  
+
+ here goes the code for the logical operators
+
+ 1. And   2. Or 3. Not
+
+*/
+
+// And operator
+
+console.log(`0 and 0 : ${0&&0}`);
+logger.log("INFO",`0 and 0 : ${0&&0}`);
+console.log(`1 and 0 : ${1&&0}`);
+logger.log("INFO",`1 and 0 : ${1&&0}`);
+console.log(`0 and 1 : ${0&&1}`);
+logger.log("INFO",`0 and 1 : ${0&&1}`);
+console.log(`1 and 1 : ${1&&1}`);
+logger.log("INFO",`1 and 1 : ${1&&1}`);

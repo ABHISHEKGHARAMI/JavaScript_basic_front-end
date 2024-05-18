4bed38f37f21d75845dc4003f154ad9aa613f71d
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-05-18 12:27:04 +0530

    Adding the logical not operator .

--- core-javascript/logicalOperators1.js
+++ core-javascript/logicalOperators1.js
@@ -31,3 +31,11 @@ console.log(`1 or 0 : ${1||0}`);
 logger.log('INFO',`1 or 0 : ${1||0}`);
 console.log(`1 or 1 : ${1||1}`);
 logger.log('INFO',`1 or 1 : ${1||1}`);
+
+
+// logical not operator
+
+let val3 = 1;
+
+console.log(`The not of the ${val3} is : ${!val3}`);
+logger.log('INFO',`The not of the ${val3} is : ${!val3}`);

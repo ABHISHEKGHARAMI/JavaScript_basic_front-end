89b7778db14b358766ac505520cc120c8cfe0795
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-05-18 13:22:10 +0530

    Adding the for loop for the repo .

---
+++ core-javascript/con_for3.js
@@ -0,0 +1,8 @@
+//  importing the logger
+const logger = require('../logger');
+
+//adding the for loop
+for(let i = 0 ;i <=50;i=i+5 ){
+  console.log(`The number is : ${i}`);
+  logger.log('INFO',`The number is : ${i}`);
+}

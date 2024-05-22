b81c2939751f15a00a403d526a24587e5352e3a7
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-05-22 15:54:08 +0530

    Adding the this program statement for the function .

--- core-javascript/func1_reverseString1.js
+++ core-javascript/func1_reverseString1.js
@@ -16,3 +16,19 @@ let str1 = 'hello there!';
 
 console.log(`The reverse of the string ${str1} is ${reverse(str1)}`);
 logger.log('INFO',`The reverse of the string ${str1} is ${reverse(str1)}`);
+
+
+//q4: Create a program to get sum of all digits of a number.
+function sum(num){
+    let rem = 0;
+    while(num!=0){
+        rem = rem * 10 + (num%10);
+        num = parseInt(num / 10);
+    }
+    return rem;
+}
+
+let num4 = 1456;
+console.log(`The sum of the number ${num4} is : ${sum(num4)}`);
+
+logger.log('INFO',`The sum of the number ${num4} is : ${sum(num4)}`);

0bd657f573bb746aabb6878183f34b732116ef20
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-05-22 15:17:43 +0530

    Adding the first function .

---
+++ core-javascript/func1_reverseString1.js
@@ -0,0 +1,18 @@
+// setting up the logger file
+const logger = require('../logger');
+
+// q4 : reverse a  string
+
+function reverse(str1){
+    str2 = "";
+    for(let i = length(str)-1;i>=0;i--){
+        str2+=str1[i];
+    }
+
+    return str2;
+}
+
+let str1 = 'hello there!';
+
+console.log(`The reverse of the string ${str1} is ${reverse(str1)}`);
+logger.log('INFO',`The reverse of the string ${str1} is ${reverse(str1)}`);

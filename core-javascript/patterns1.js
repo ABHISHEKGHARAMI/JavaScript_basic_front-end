4033b2123eeadb28c258f23a93bf5ebb74fd6adf
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-05-25 12:04:51 +0530

    Adding the patterns for the repo .

---
+++ core-javascript/patterns1.js
@@ -0,0 +1,13 @@
+// printing the pattern for the js
+const logger = require('../logger');
+
+// printing the patterns
+
+let i,j;
+
+for(i=0;i<=5;i++){
+    for(j=0;j<=i;j++){
+        console.log('*');
+    }
+    console.log("\n");
+}

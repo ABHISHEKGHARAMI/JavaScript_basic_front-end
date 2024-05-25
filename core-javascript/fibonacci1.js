324a1afda9aed12803fce324ea2dc5b2897908ca
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-05-25 13:34:35 +0530

    fibonacci series .

--- core-javascript/fibonacci1.js
+++ core-javascript/fibonacci1.js
@@ -3,7 +3,23 @@ const logger = require('../logger');
 
 
 function fibonacci(n){
- if(n==0){
-    return 0;
+ let a = 0;
+ let b = 1;
+ if (n == 0 || n == 1){
+    return a;
+ }
+ else{
+    return fibonacci(n-1) + fibonacci(n-2)
  }
 }
+
+let n = 12;
+
+console.log(`the fibonacci series is`);
+logger.log(`the fibonacci series is :`);
+
+let a = fibonacci(n);
+
+console.log(`${a}`);
+logger.log(`${a}`);
+

57cc115a3c8c76dd5bc62e42b66b1193a644d187
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-05-28 11:09:55 +0530

    .mod

--- core-javascript/function_call1.js
+++ core-javascript/function_call1.js
@@ -21,9 +21,9 @@ function isPrime(num1){
 function sumPrime(num1){
     let sum = 0;
 
-    for(let i=0;i<parseInt((num1)/2);i++){
+    for(let i=2;i<parseInt((num1)/2);i++){
         if(isPrime(i)==0 && isPrime(num1-i)==0){
-            sum += i + (num-i);
+            sum += i + (num1-i);
         }
     }
 

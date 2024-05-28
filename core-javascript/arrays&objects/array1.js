5678a03deb1e5e0fff2cc6076da4924e701a95b8
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-05-28 14:54:01 +0530

    .mod

--- core-javascript/arrays&objects/array1.js
+++ core-javascript/arrays&objects/array1.js
@@ -63,10 +63,10 @@ logger.log('INFO',`the new array will be : ${arr2}`);
 
 // now concat
 
-arr1.concat(arr2);
+let arr3 = arr1.concat(arr2);
 
-console.log(`After concating with  the first array with the second one is : ${arr1}`);
-logger.log('INFO',`After concating with  the first array with the second one is : ${arr1}`);
+console.log(`After concating with  the first array with the second one is : ${arr3}`);
+logger.log('INFO',`After concating with  the first array with the second one is : ${arr3}`);
 
-console.log(`the new length will be : ${arr1.length}`);
-logger.log('INFO',`the new length will be : ${arr1.length}`);
+console.log(`the new length will be : ${arr3.length}`);
+logger.log('INFO',`the new length will be : ${arr3.length}`);

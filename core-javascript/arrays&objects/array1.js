07f45fe9d072c9675c30876b8b7c1bb47eba3fbb
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-05-28 14:23:37 +0530

    .mod

--- core-javascript/arrays&objects/array1.js
+++ core-javascript/arrays&objects/array1.js
@@ -11,3 +11,8 @@ arr1.push('react.js');
 
 console.log(`the array is :${arr1}`);
 logger.log('INFO',`the array is :${arr1}`);
+
+let len1 = arr1.length();
+
+console.log(`the length of the array is : ${len1}`);
+logger.log('INFO',`the length of the array is : ${len1}`);

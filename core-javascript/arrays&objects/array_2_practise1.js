b864c99731dec36dbbff3f8e9df802bc938a4f61
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-06-02 18:24:22 +0530

    Adding the second array .

---
+++ core-javascript/arrays&objects/array_2_practise1.js
@@ -0,0 +1,15 @@
+// practice of the array
+
+const logger = require('../../logger');
+
+let arr3 = new Array(5);
+
+arr3.push('a');
+arr3.push('b');
+arr3.push('c');
+arr3.push('d');
+arr3.push('e');
+
+console.log(`the array is : ${arr3}`);
+logger.log('INFO',`the array is : ${arr3}`);
+

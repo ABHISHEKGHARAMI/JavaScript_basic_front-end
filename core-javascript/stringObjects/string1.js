6d68349c3ad8c3f89b076fd4329e9c45917fccb4
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-06-05 21:57:58 +0530

    the mod

--- core-javascript/stringObjects/string1.js
+++ core-javascript/stringObjects/string1.js
@@ -4,3 +4,6 @@ const logger = require('../../logger');
 let str1  = 'this is a string.';
 
 console.log(`the type `);
+
+
+// from today we will resume to the dev .
---
+++ core-javascript/stringObjects/string2.js
@@ -0,0 +1,6 @@
+const logger = require('../../logger');
+
+let str1 = new String('hello there i am starting the javascript..');
+
+console.log(`the string is : ${str1}`);
+logger.log('INFO',`the string is : ${str1}`);

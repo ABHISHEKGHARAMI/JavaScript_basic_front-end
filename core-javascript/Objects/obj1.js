44a4217ad8ceafa71d8445d7763cc25e171ef8b3
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-06-20 21:09:09 +0530

    .mod

--- core-javascript/Objects/obj1.js
+++ core-javascript/Objects/obj1.js
@@ -12,6 +12,7 @@ let obj1 = {
     },
     func_prop : function(){
         console.log('Welcome to geeksforgeeks');
+        logger.log('INFO','Welcome to geeksforgeeks');
     }
 }
 
@@ -28,3 +29,5 @@ logger.log('INFO',`Object Property : ${obj1.obj_prop}`);
 
 console.log(`Inner property : ${obj1.inner_prop.int_prop}`);
 logger.log('INFO',`Inner property : ${obj1.inner_prop.int_prop}`);
+
+obj1.func_prop();

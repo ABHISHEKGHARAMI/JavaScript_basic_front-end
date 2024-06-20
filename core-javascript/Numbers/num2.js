5700aa41da4059ba2322cc0e5a87d7f07dc336ba
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-06-20 20:44:30 +0530

    .mod

--- core-javascript/Numbers/num2.js
+++ core-javascript/Numbers/num2.js
@@ -4,9 +4,9 @@ const logger = require('../../logger');
 
 // doing the math project
 console.log('Math.Ln10'+Math.LN10);
-logger.log('Math.ln10 : '+ Math.LN10);
+logger.log('INFO','Math.ln10 : '+ Math.LN10);
 
 // ln2
 console.log('Math.ln2',Math.LN2);
-logger.log('Math.ln2',Math.LN2);
+logger.log('INFO','Math.ln2',Math.LN2);
 

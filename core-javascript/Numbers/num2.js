15c2cae2a2328170d5732c342c802089d38a533a
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-06-20 20:47:41 +0530

    Adding the pi .

--- core-javascript/Numbers/num2.js
+++ core-javascript/Numbers/num2.js
@@ -10,3 +10,7 @@ logger.log('INFO','Math.ln10 : '+ Math.LN10);
 console.log('Math.ln2',Math.LN2);
 logger.log('INFO','Math.ln2',Math.LN2);
 
+// pi 
+console.log('Math.pi',Math.PI);
+logger.log('INFO',`math.pi : ${Math.PI}`);
+

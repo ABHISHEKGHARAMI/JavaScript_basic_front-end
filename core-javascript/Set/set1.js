419c6dc771e288a415cee37e63d09f6cb9e2fc2c
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-06-23 11:04:54 +0530

    Adding the set value for the set .

--- core-javascript/Set/set1.js
+++ core-javascript/Set/set1.js
@@ -11,3 +11,20 @@ let set2 = new Set([10,10,10,20,20,30,30,40,40]);
 
 console.log(set2);
 logger.log('INFO',set2);
+
+
+// adding the new values for the set
+set1.add('james');
+set1.add('Charles');
+set1.add('longing');
+
+set2.add(50);
+set2.add(50);
+set1.add(40);
+
+//updating the set for different value
+console.log(set1);
+logger.log('INFO',set1);
+
+console.log(set2);
+logger.log('INFO',set2);

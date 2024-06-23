a2f6553c84bd6c6a5d06ea7401b61474a7011b8c
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-06-23 10:24:30 +0530

    Adding the map .

---
+++ core-javascript/Map/map1.js
@@ -0,0 +1,14 @@
+// setting up the logger file
+const logger = require('../../logger');
+
+// creating map
+let map1 = new Map(
+    [1,10],
+    [2,20],[3,56],
+    [5,90]
+);
+
+
+// executing the map for the js
+console.log(`After the map : ${map1}`);
+logger.log('INFO',`After the map : ${map1}`);

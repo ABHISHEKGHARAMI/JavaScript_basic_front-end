fdca8d4cb1b20379de76fc34a55e56d8f6c732d9
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-06-23 10:56:00 +0530

    .mod

--- core-javascript/Set/set1.js
+++ core-javascript/Set/set1.js
@@ -5,3 +5,9 @@ let set1 = new Set(['sumit','sumit','allo']);
 
 console.log(set1);
 logger.log('INFO',set1);
+
+
+let set2 = new Set([10,10,10,20,20,30,30,40,40]);
+
+console.log(set2);
+logger.log('INFO',set2);
